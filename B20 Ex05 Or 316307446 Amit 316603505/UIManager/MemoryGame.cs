14c039f8eda6381816414477091f145bafdc2a34
Amit Shmuelevitz <amit1shmu@gmail.com>
2020-07-10 16:29:16 +0300

    zibi

--- B20 Ex05 Or 316307446 Amit 316603505/UIManager/MemoryGame.cs
+++ B20 Ex05 Or 316307446 Amit 316603505/UIManager/MemoryGame.cs
@@ -37,7 +37,7 @@ namespace UIManager
             // 
             // MemoryGame
             // 
-            this.ClientSize = new System.Drawing.Size(747, 451);
+            //this.ClientSize = new System.Drawing.Size(747, 451);
             this.Name = "MemoryGame";
             this.Text = "Memory Game";
             this.ResumeLayout(false);
@@ -59,7 +59,7 @@ namespace UIManager
                     global::MemoryGame.Square currentSquare = m_BoardGame.BoardGameWithSquares.m_SuqaresValue[i, j];
                     m_BoardGame.BoardGameWithButtons[i, j] = new MemoryGameButton(currentSquare);
                     m_BoardGame.BoardGameWithButtons[i, j].Click += new EventHandler(ButtonClicked);
-                    m_BoardGame.BoardGameWithButtons[i, j].Size = new System.Drawing.Size(100, 90);
+                    m_BoardGame.BoardGameWithButtons[i, j].Size = new System.Drawing.Size(80, 80);
                     m_BoardGame.BoardGameWithButtons[i, j].Name = "Card" + i + j;
                     m_BoardGame.BoardGameWithButtons[i, j].Text = string.Empty;
                     m_BoardGame.BoardGameWithButtons[i, j].UseVisualStyleBackColor = true;
@@ -75,7 +75,7 @@ namespace UIManager
                         // need to change 0 const
                         if (i == 0)
                         {
-                            int ButtonXLocation = m_BoardGame.BoardGameWithButtons[i, j - 1].Location.X + m_BoardGame.BoardGameWithButtons[i, j].Right + 24;
+                            int ButtonXLocation = m_BoardGame.BoardGameWithButtons[i, j - 1].Location.X + m_BoardGame.BoardGameWithButtons[i, j].Right + 12;
                             int ButtonYLocation = m_BoardGame.BoardGameWithButtons[i, j - 1].Location.Y;
                             m_BoardGame.BoardGameWithButtons[i, j].Location = new System.Drawing.Point(ButtonXLocation, ButtonYLocation);
                         }
@@ -85,13 +85,13 @@ namespace UIManager
                             if (j == 0)
                             {
                                 int ButtonXLocation = m_BoardGame.BoardGameWithButtons[i - 1, j].Location.X;
-                                int ButtonYLocation = m_BoardGame.BoardGameWithButtons[i - 1, j].Location.Y + m_BoardGame.BoardGameWithButtons[i, j].Bottom + 24;
+                                int ButtonYLocation = m_BoardGame.BoardGameWithButtons[i - 1, j].Location.Y + m_BoardGame.BoardGameWithButtons[i, j].Bottom + 12;
                                 m_BoardGame.BoardGameWithButtons[i, j].Location = new System.Drawing.Point(ButtonXLocation, ButtonYLocation);
                             }
                             else
                             {
-                                int ButtonXLocation = m_BoardGame.BoardGameWithButtons[i, j - 1].Location.X + m_BoardGame.BoardGameWithButtons[i, j].Right + 24;
-                                int ButtonYLocation = m_BoardGame.BoardGameWithButtons[i - 1, j].Location.Y + m_BoardGame.BoardGameWithButtons[i, j].Bottom + 24;
+                                int ButtonXLocation = m_BoardGame.BoardGameWithButtons[i, j - 1].Location.X + m_BoardGame.BoardGameWithButtons[i, j].Right + 12;
+                                int ButtonYLocation = m_BoardGame.BoardGameWithButtons[i - 1, j].Location.Y + m_BoardGame.BoardGameWithButtons[i, j].Bottom + 12;
                                 m_BoardGame.BoardGameWithButtons[i, j].Location = new System.Drawing.Point(ButtonXLocation, ButtonYLocation);
                             }
                         }
@@ -104,12 +104,11 @@ namespace UIManager
             // 
             // m_CurrentPlayersTurn
             //
-            /*
-            System.Drawing.Point temp = m_BoardGame.BoardGameWithButtons[0, 3].Location;
+            
             this.m_CurrentPlayersTurn.AutoSize = true;
-            int XLocation = m_BoardGame.BoardGameWithButtons[0, 0].Location.X;
-            int YLocation = m_BoardGame.BoardGameWithButtons[3, 0].Location.Y + 20;
-            this.m_CurrentPlayersTurn.Location = new System.Drawing.Point(XLocation, YLocation);
+            int XLocationCurrent = m_BoardGame.BoardGameWithButtons[0, 0].Location.X;
+            int YLocationCurrent = m_BoardGame.BoardGameWithButtons[m_NumOfRows - 1, 0].Bottom + 10;
+            this.m_CurrentPlayersTurn.Location = new System.Drawing.Point(XLocationCurrent, YLocationCurrent);
             this.m_CurrentPlayersTurn.Name = "m_CurrentPlayersTurn";
             this.m_CurrentPlayersTurn.Size = new System.Drawing.Size(137, 20);
             this.m_CurrentPlayersTurn.TabIndex = 0;
@@ -120,7 +119,9 @@ namespace UIManager
             // m_FirstPlayerScore
             // 
             this.m_FirstPlayerScore.AutoSize = true;
-            this.m_FirstPlayerScore.Location = new System.Drawing.Point(m_CurrentPlayersTurn.Location.X, m_CurrentPlayersTurn.Location.Y + 20);
+            int XLocationFirst = m_CurrentPlayersTurn.Location.X;
+            int YLocationFirst = m_CurrentPlayersTurn.Bottom + 10;
+            this.m_FirstPlayerScore.Location = new System.Drawing.Point(XLocationFirst, YLocationFirst);
             this.m_FirstPlayerScore.Name = "m_FirstPlayerScore";
             this.m_FirstPlayerScore.Size = new System.Drawing.Size(137, 20);
             this.m_FirstPlayerScore.TabIndex = 0;
@@ -130,7 +131,9 @@ namespace UIManager
             // m_SecondPlayerScore
             // 
             this.m_SecondPlayerScore.AutoSize = true;
-            this.m_SecondPlayerScore.Location = new System.Drawing.Point(m_FirstPlayerScore.Location.X, m_FirstPlayerScore.Location.Y + 20);
+            int XLocationSecond = m_FirstPlayerScore.Location.X;
+            int YLocationSecond = m_FirstPlayerScore.Bottom + 10;
+            this.m_SecondPlayerScore.Location = new System.Drawing.Point(XLocationSecond, YLocationSecond);
             this.m_SecondPlayerScore.Name = "m_SecondPlayerScore";
             this.m_SecondPlayerScore.Size = new System.Drawing.Size(137, 20);
             this.m_SecondPlayerScore.TabIndex = 0;
@@ -139,7 +142,11 @@ namespace UIManager
             this.Controls.Add(m_CurrentPlayersTurn);
             this.Controls.Add(m_FirstPlayerScore);
             this.Controls.Add(m_SecondPlayerScore);
-            */
+
+            int XWindowSize = m_BoardGame.BoardGameWithButtons[m_NumOfRows - 1, m_NumOfColums - 1].Right + 12;
+            int YWindowSize = m_SecondPlayerScore.Bottom + 12;
+            this.ClientSize = new System.Drawing.Size(XWindowSize, YWindowSize);
+
         }
 
         private void ButtonClicked(object sender, EventArgs e)
